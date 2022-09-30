0feaa1f7134e04f1c8b57aa6e9ca5cfd7108782a
ahkamulbd <ahkamulbd@gmail.com>
2022-09-30 12:10:15 +0600

    Problem 2 done in recapping Assignment 4

---
+++ problem2.js
@@ -0,0 +1,18 @@
+/*
+Problem:2  isJavaScriptFile 
+
+    ফাংশন নেম দিতে হবে isJavaScriptFile । এই ফাংশনে প্যারামিটার হিসেবে নিবে একটি স্ট্রিং(String) যেটি হবে একটি ফাইল নেম (যেমনঃ ‘index.js’)। যদি এটি জাভাস্ক্রিপ্ট ফাইল হয় তোমাকে true রিটার্ন করতে হবে আর যদি না হয় তাহলে false রিটার্ন করতে হবে । 
+*/
+
+//let stringName = 'index.js';
+function isJavaScriptFile(stringName) {
+    if (stringName.endsWith('.js') === true) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+const result = isJavaScriptFile('image.png.js');
+console.log(result);
